d06e44581c468808e3f0c87eca83002360889fcf
François <francois.ezard@gmail.com>
2024-04-19 12:26:52 +0200

    bug fix working

--- freedomOfComputing/src/main/java/nl/tudelft/trustchain/foc/MainActivityFOC.kt
+++ freedomOfComputing/src/main/java/nl/tudelft/trustchain/foc/MainActivityFOC.kt
@@ -109,7 +109,6 @@ open class MainActivityFOC : AppCompatActivity() {
             focCommunity?.activity = this
             appGossiper = focCommunity?.let { AppGossiper.getInstance(s, this, it, true) }
             appGossiper?.start()
-            onResume()
         } catch (e: Exception) {
             printToast(e.toString())
         }
@@ -162,6 +161,8 @@ open class MainActivityFOC : AppCompatActivity() {
         resumeUISettings()
         appGossiper?.resume()
         voteTracker.loadState(cacheDir.absolutePath + "/vote-tracker" + DATA_DOT_EXTENSION)
+        val files = applicationContext.cacheDir.listFiles()
+        files?.forEach { file -> updateVoteCounts(file.name) }
         val ids = HashSet<UUID>()
         voteTracker.getCurrentState().forEach { (_, u) -> u.forEach { vote -> ids.add(vote.id) } }
         focCommunity?.sendPullRequest(ids)
